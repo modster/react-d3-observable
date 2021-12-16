a77763c590606c334c3c6b6c4d876070e51cdc36
E. M. Greeff <unmodster@gmail.com>
2021-12-15 23:46:34 -0700

    import candlestick chart

--- src/App.js
+++ src/App.js
@@ -1,25 +1,29 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useRef, useEffect } from "react";
+import { Runtime, Inspector } from "@observablehq/runtime";
+import notebook from "@modster/candlestick-chart";
 
-function App() {
+function CandlestickChart() {
+  const chartRef = useRef();
+
+  useEffect(() => {
+    const runtime = new Runtime();
+    runtime.module(notebook, (name) => {
+      if (name === "chart") return new Inspector(chartRef.current);
+    });
+    return () => runtime.dispose();
+  }, []);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
+    <>
+      <div ref={chartRef} />
+      <p>
+        Credit:{" "}
+        <a href="https://observablehq.com/@modster/candlestick-chart">
+          Candlestick Chart by MichaelGreeff
         </a>
-      </header>
-    </div>
+      </p>
+    </>
   );
 }
 
-export default App;
+export default CandlestickChart;
